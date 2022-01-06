90de5479773d4fef0af54d0e7acb6e05772557dd
Knoa <mike1124@naver.com>
2022-01-07 00:02:49 +0900

    Feat: Add h1 tag click event callback

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log('hello');
+const h1Tag: HTMLHeadingElement | null  = document.querySelector('h1');
+
+function handleClick(e: Event) {
+    console.log(e.target);
+}
+
+h1Tag?.addEventListener('click',(e) => handleClick(e))
