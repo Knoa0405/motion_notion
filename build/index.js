aa03d03cc3dfe316b5f8f3c82ee91d2910d84a12
Knoa <mike1124@naver.com>
2022-01-09 22:15:58 +0900

    fix: debug js file load defer in html file

--- build/index.js
+++ build/index.js
@@ -1,2 +1,8 @@
 "use strict";
-console.log('hello');
+var body = document.getElementsByTagName('body')[0];
+var h1Tag = body.querySelector('h1.title');
+function handleClick(e) {
+    console.log(e.target);
+    console.log(e);
+}
+h1Tag.addEventListener('click', function (e) { return handleClick(e); });
