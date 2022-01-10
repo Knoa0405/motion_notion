34f066617255889fe9159d20628ac0e26ac2151c
Knoa <mike1124@naver.com>
2022-01-10 23:50:09 +0900

    feat: add list function for reusable

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@ const h1Tag: HTMLHeadingElement = body.querySelector('h1.title')!;
 
 function handleClick(e: Event) {
     console.log(e.target);
-    console.log(e);
 }
 
-h1Tag.addEventListener('click',(e) => handleClick(e))
+h1Tag.addEventListener('click',(e) => handleClick(e));
