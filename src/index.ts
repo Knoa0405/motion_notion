e7c4de5cdd49785830dfec31e02ee166fa3b5b06
Knoa <mike1124@naver.com>
2022-01-11 22:55:54 +0900

    Chore: Add babel/webpack for bundling files

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,1 @@
 const body: HTMLBodyElement = document.getElementsByTagName('body')[0];
-
-const h1Tag: HTMLHeadingElement = body.querySelector('h1.title')!;
-
-function handleClick(e: Event) {
-    console.log(e.target);
-}
-
-h1Tag.addEventListener('click',(e) => handleClick(e));
