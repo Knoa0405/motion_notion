6a78ddb9ccb7f73a8ae42d084d389dd88902fe97
Knoa <mike1124@naver.com>
2022-01-14 23:59:02 +0900

    chore : Set up tailwind css with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
@@ -10,13 +11,26 @@ module.exports = {
         use: 'ts-loader',
         exclude: /node_modules/,
       },
+      {
+        test: /\.css$/i,
+        include: path.resolve(__dirname, 'src'),
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
     ],
   },
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
   },
   output: {
-    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/',
+    clean: true,
   },
+  devServer: {
+    static: { directory: path.join(__dirname, 'dist') },
+    open: true,
+    port: 'auto',
+  },
+  plugins: [new HtmlWebpackPlugin({ template: 'src/index.html' })],
 };
