7fd549a55eee5cb0e22afa537b9b5c69198ae94d
Knoa <mike1124@naver.com>
2022-01-15 19:58:32 +0900

    feat: Add classify category type

---
+++ dist/utils/types.js
@@ -0,0 +1,79 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (function() { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/utils/types.ts":
+/*!****************************!*\
+  !*** ./src/utils/types.ts ***!
+  \****************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _types_json__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./types.json */ \"./src/utils/types.json\");\n\r\nfunction classificationCategoryTypes(categoryNumber) {\r\n    var types = _types_json__WEBPACK_IMPORTED_MODULE_0__;\r\n    return types[categoryNumber];\r\n}\r\n/* harmony default export */ __webpack_exports__[\"default\"] = (classificationCategoryTypes);\r\n\n\n//# sourceURL=webpack://motion_notion/./src/utils/types.ts?");
+
+/***/ }),
+
+/***/ "./src/utils/types.json":
+/*!******************************!*\
+  !*** ./src/utils/types.json ***!
+  \******************************/
+/***/ (function(module) {
+
+eval("module.exports = JSON.parse('{\"IMAGE\":0,\"VIDEO\":1,\"TEXT\":2,\"TASK\":3}');\n\n//# sourceURL=webpack://motion_notion/./src/utils/types.json?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	!function() {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = function(exports) {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	}();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/utils/types.ts");
+/******/ 	
+/******/ })()
+;
--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,15 @@ import ImplItem  from "./Item";
 
 const buttons: HTMLElement = document.querySelector('.buttons')!;
 
-function handleClick(e : Event) : void {
-    ImplItem({ parentElement : buttons });
+interface DOMEvent<T extends HTMLElement> extends MouseEvent {
+    readonly target : T;
+}
+
+function handleClick(e: MouseEvent) : void {
+    const event = e as DOMEvent<HTMLButtonElement>;
+    const item = new ImplItem(buttons);
+
+    console.log(item.createItem(event.target.innerText));
 }
 
 buttons.addEventListener('click', (e) => handleClick(e));
