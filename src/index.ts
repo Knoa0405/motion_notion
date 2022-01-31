1054a66866511445912569c19075136b84434423
Knoa <mike1124@naver.com>
2022-02-01 04:42:52 +0900

    Feat : Add each a different property list if user click buttons in each types

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import './tailwind.css';
 import ImplItem  from "./Item";
 
 const buttons: HTMLElement = document.querySelector('.buttons')!;
+const uList: HTMLElement = document.querySelector('ul.list')!;
+
 
 interface DOMEvent<T extends HTMLElement> extends MouseEvent {
     readonly target : T;
@@ -10,9 +12,8 @@ interface DOMEvent<T extends HTMLElement> extends MouseEvent {
 
 function handleClick(e: MouseEvent) : void {
     const event = e as DOMEvent<HTMLButtonElement>;
-    const item = new ImplItem(buttons);
-
-    console.log(item.createItem(event.target.innerText));
+    const item = new ImplItem(uList);
+    item.createItem(event.target.innerText);
 }
 
 buttons.addEventListener('click', (e) => handleClick(e));
