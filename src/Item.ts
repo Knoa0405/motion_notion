d50d44cd5d69f9754cb9029c5a086e705f4ab308
Knoa0405 <mike1124@naver.com>
2022-02-12 22:01:26 +0900

    Composition - Implements DI in TS class => list class - props : type, method : getList

--- src/Item.ts
+++ src/Item.ts
@@ -1,15 +1,15 @@
-// 1. 각 종류별 리스트는 모두 CRUD 가 가능해야 한다.  
+// 1. 각 종류별 리스트는 모두 CRUD 가 가능해야 한다.
 // 1-1. 각 리스트에는 해당 리스트 등록 버튼과 기능이 필요하다.  
 // 1-2. 각 리스트에는 수정 버튼과 기능이 필요하다.
 // 1-3. 각 리스트에는 삭제 버튼과 기능이 필요하다.  
 
-import getListFromType, { Type } from "utils/getListFromType";
+import ImplList from "./List";
 import classificationCategoryTypes from "utils/types";
 
 // 1-4. 각 리스트에는 텍스트 등록 인풋이 있다. ( 리스트 title 넣는 부분 )
 interface Item {
     parentElement : HTMLElement;
-    createItem(type: string): void;
+    createItem(implList: ImplList): void;
     updateItem() : void;
     deleteItem() : void;
 }
@@ -20,10 +20,12 @@ class ImplItem implements Item {
     constructor(parentElement: HTMLElement) {
         this.parentElement = parentElement
     }
-
-    createItem = (categoryName: string) : void => {
-        const type: number = classificationCategoryTypes(categoryName);
-       this.parentElement.innerHTML = getListFromType(type as Type);
+    // 아이템 생성시
+    // 1. 이미지 - 이미지 파일 업로드, url 삽입
+    // 2. 비디오 - 비디오 url 삽입
+    // 3. 테스크 - 할일 목록
+    createItem = (implList: ImplList) : void => {
+       this.parentElement.innerHTML = implList.getList();
         // type check 후 type 별로 다른 특성을 가진 리스트를 생성해준다.
     }
 
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import './tailwind.css';
 
 import ImplItem  from "./Item";
+import ImplList from './List';
+import classificationCategoryTypes from 'utils/types';
 
 const buttons: HTMLElement = document.querySelector('.buttons')!;
 const uList: HTMLElement = document.querySelector('ul.list')!;
@@ -13,7 +15,9 @@ interface DOMEvent<T extends HTMLElement> extends MouseEvent {
 function handleClick(e: MouseEvent) : void {
     const event = e as DOMEvent<HTMLButtonElement>;
     const item = new ImplItem(uList);
-    item.createItem(event.target.innerText);
+    
+    const type: number = classificationCategoryTypes(event.target.innerText);
+    item.createItem(new ImplList(type));
 }
 
 buttons.addEventListener('click', (e) => handleClick(e));
